e26924f7c63fd994973b0ae1db2641453a9f4a7b
Ben Hutton <ben.hutton@cinglevue.com>
2017-02-09 08:45:41 +0800

    improved animation speed

--- src/AnimateTest/Animate.java
+++ src/AnimateTest/Animate.java
@@ -1,79 +1,76 @@
 package AnimateTest;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.geom.*;
-import java.util.concurrent.TimeUnit;
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-import javax.swing.JApplet;
-import javax.swing.JFrame;
-import javax.swing.JTextField;
-
-public class Animate extends JApplet {
+public class Animate extends Applet implements Runnable {
 	
 	int x,y,num;
-
-	public Animate(){
-		
-		setSize(400,400);
-		
-		//this.x   = 2387;
-		//this.y   = 45;
-		this.num = 2343;
+	
+	int delay = 100;
+	
+	Thread animatorThread;
+	
+	boolean frozen = false;
+	
+	public void init(){
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+	        
+				if (frozen) {
+			          frozen = false;
+			          start();
+				} 
+				else {
+					frozen = true;
+					stop();
+				}
+			}
+		});
 	}
 	
-	public void paint(Graphics g)
-	{
-		int count;		// Loop counter
-		
-		int x = 0;
-		int y = 0;
-		int prevX = 0;
-		int prevY = 0;
-		
-		for(count = 0; count < num; count++) {
-			
-			super.paint(g);
-			
-			//g.clearRect(prevX, prevY, 100, 100);
-			
-			// Recalculate destination coordinates
-			y += 1;
-			x += 1;
-			
-			// Calculate random colours
-			//int R = (int)(Math.random() * 256);
-			//int G = (int)(Math.random() * 256);
-			//int B = (int)(Math.random() * 256);
-			
-			// Get new color based on random numbers
-			//Color randomColor = new Color(R, G, B);
-			
-			setSize(400,400);
-			setBackground(Color.blue);
-		
-			
-			// Set new color
-			//g.setColor(randomColor);
-			g.setColor(Color.blue);
-			
-			// Draw line
-			
-
-			g.drawRect(x, y, 100, 100);
-			
-			prevX = x;
-			prevY = y;
-			
-			try {
-				TimeUnit.SECONDS.sleep(1);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	public void start() {
+		if (!frozen) {
+			if (animatorThread == null) {
+				animatorThread = new Thread(this);
 			}
 			
-			
-				
+			animatorThread.start();
 		}
 	}
+
+	public void stop() {
+	    animatorThread = null;
+	}
+
+	public void run() {
+		
+		Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+		
+		long startTime = System.currentTimeMillis();
+		
+		Thread currentThread = Thread.currentThread();
+
+	    while (currentThread == animatorThread) {
+	    	x++;
+	    	y++;
+
+	    	repaint();
+	    	
+	    	try {
+	    		startTime += delay;
+	    		Thread.sleep(100);
+	    	} 
+	    	catch (InterruptedException e) {
+	    		break;
+	    	}
+	    }
+	}
+
+	public void paint(Graphics g) {
+	    g.drawRect(x, y, 100, 100);
+	}
+
 }
