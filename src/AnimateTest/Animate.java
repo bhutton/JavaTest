35de5684492a72948fc52efa7188a09d8e45a322
Ben Hutton <ben.hutton@cinglevue.com>
2017-02-09 13:27:15 +0800

    now bounces around the screen.

--- src/AnimateTest/Animate.java
+++ src/AnimateTest/Animate.java
@@ -1,27 +1,33 @@
 package AnimateTest;
 
 import java.applet.Applet;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Animate extends Applet implements Runnable {
 	
-	int x,y,num;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	int x,y,num,width=100,height=100, appletHeight, appletWidth, incX=1, incY=1;
 	
 	int delay = 100;
 	
 	Thread animatorThread;
 	
-	boolean frozen = false;
+	boolean frozen = false, forward = true, right = true, down = true;
 	
 	public void init(){
 		addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) {
 	        
 				if (frozen) {
-			          frozen = false;
-			          start();
+					frozen = false;
+					start();
 				} 
 				else {
 					frozen = true;
@@ -54,23 +60,43 @@ public class Animate extends Applet implements Runnable {
 		Thread currentThread = Thread.currentThread();
 
 	    while (currentThread == animatorThread) {
-	    	x++;
-	    	y++;
+	    	
+	    	calculateLocation();	
 
 	    	repaint();
 	    	
 	    	try {
 	    		startTime += delay;
-	    		Thread.sleep(100);
+	    		Thread.sleep(10);
 	    	} 
 	    	catch (InterruptedException e) {
 	    		break;
 	    	}
 	    }
 	}
+	
+	public void calculateLocation() {
+		
+		if ((x + width + 2) > appletWidth) right = false;
+		if ((y + height + 2) > appletHeight) down = false;
+    	if (x == 0) right = true; 
+    	if (y == 0) down = true; 
+    	
+    	
+    	
+    	if (right) x += incX; 
+    	else x -= incX;
+    	if (down ) y += incY;
+    	else y -= incY;
+    	
+	}
 
 	public void paint(Graphics g) {
-	    g.drawRect(x, y, 100, 100);
+		Dimension appletSize = this.getSize();
+	    appletHeight = appletSize.height;
+	    appletWidth = appletSize.width;
+	    
+	    g.drawOval(x, y, width, height);
 	}
 
 }
